5326fe70d87c8b94214cdfa3fa61b39f0093db84
Paul Andre Rada <paulriverarada@gmail.com>
2014-08-27 14:59:41 -0700

    Added the files from the previous site

---
+++ js/main.js
@@ -0,0 +1,59 @@
+//
+//
+// @codekit-prepend "plugins.js";
+
+
+
+$(function(){ 	
+	'use strict';
+	/*
+	var $doc = $(document),
+	    $introCon = $doc.find( '#introContainer' ),
+	    $contentCon = $doc.find( '#contentCon' ),
+	    $decorations = $doc.find( '#tear' ),
+	    desktopIntro = 'intro_container_desktop',
+	    mobileIntro = 'intro_container_mobile',
+	    desktopContent = 'content_container_desktop',
+	    mobileContent = 'content_container_mobile';
+	
+	enquire.register("screen and (max-width: 767px)", { 
+		//
+		match : function(){ 
+		    // 
+		    // change the intro case node when in mobile size
+		    $introCon.addClass( mobileIntro )
+			.removeClass( desktopIntro );
+		    //
+		    // change the content_container's margins
+		    $contentCon.addClass( mobileContent )
+			.removeClass( desktopContent );
+		    //
+		    //
+		    // hide the decorations
+		    $decorations.hide();
+		    //
+		},unmatch : function(){ 
+		    //
+		    // change back to the desktop mode
+		    $introCon.addClass( desktopIntro )
+			.removeClass( mobileIntro );
+		    //
+		    //
+		    $contentCon.addClass( desktopContent )
+		      .removeClass( mobileContent );
+		    //
+		    // show the decorations
+		    $decorations.show()
+		    //
+		}
+		//
+	    });
+	
+	
+	
+	*/
+	
+	
+	
+    });
+
