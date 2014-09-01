7fb985a4d3cd637993630cc04182ffe68fd95fa6
Paul Andre Rada <paulriverarada@gmail.com>
2014-08-31 20:49:16 -0700

    -Created the Vertical Carousel object in the main.js file -Added the Hammer.2.0.js minimized code into the plugins.js file -Currently resetting the project to used compass -( for some unkown reason it disconected itself )

--- js/main.js
+++ js/main.js
@@ -99,6 +99,154 @@ $(function(){
 	// just the project images and descriptions left
 
 
+	// @carousel
+	
+	var verticalCarousel = function( element ){ 
+	    
+	    var VC = this,
+	    $doc = $( document ),
+	    $element = $doc.find( element ), // div on top of rails
+	    $container = $element.children(), // would be rails
+	    $panes = $container.filter( 'section' ), // sections
+	    paneHeight = 0,
+	    paneCount = $panes.length,
+	    currentPane = 0;
+	    
+	    VC.init = function(){ 
+		//
+		VC.setDimensions();
+		//
+		$(window).on("load resize orientationchange", 
+			     function() {
+				 VC.setPaneDimensions();
+			     });
+		//
+		return VC;
+	    };
+
+	    
+	    VC.setPaneDimensions = function(){ 
+		//
+		paneHeight = $element.height();
+		//
+		for ( var i; i<paneCount; i++ ){ 
+		    // 
+		    $panes.height( paneHeight );
+
+		};
+		//
+		$container.height( paneHeight * paneCount );
+		//
+		return VC;
+	    };
+	    
+	    
+	    VC.showPane = function( index ) { 
+		//
+		// prevent going under or over
+		var index = Math.max( 0, Math.min( index,
+						   paneCount -1 )),
+		//
+		// 
+		currentPane = index;
+		// 
+		// create the offset value for the container
+		// starting at 0
+		var offset = -(( 100/paneCount ) * currentPane );
+		//
+		VC.setContainerOffset( offset, true );
+		//
+		return VC;
+	    };
+	    
+	    VC.setContainerOffset = function( percent, animate ){ 
+		//
+		$container.removeClass( "animate" );
+		//
+		if ( animate ) { 
+		    $container.addClass( "animate");
+		}
+		//
+		if(Modernizr.csstransforms3d) {
+		    container.css("transform", 
+				  "translate3d(0,"+ percent +
+				  "%,0) scale3d(1,1,1)");
+		} else if(Modernizr.csstransforms) {
+		    container.css("transform", 
+				  "translate(0,"+ percent +"%)");
+		} else {
+		    var px = ((pane_width*pane_count) / 100) 
+			* percent;
+		    //
+		    container.css("top", px+"px");
+		}
+		//
+		return VC;
+	    };
+	    
+	    VC.next = function(){ 
+		// 
+		return VC.showPane( currentPane + 1 );
+		//
+	    };
+
+	    VC.prev = function(){ 
+		//
+		return VC.showPane( currentPane - 1);
+		//
+	    };
+		
+	    var handleTouch = function( ev ){ 
+		//
+		// prevent scrolling
+		ev.gesture.preventDefault();
+		//
+		switch( ev.type ){ 
+		    //
+		case 'swipeup': 
+		    //
+		    VC.next();
+		    // stop detecting gestures
+		    ev.gesture.stopDetect();
+		    break;
+		    //
+		case 'swipedown' : 
+		    //
+		    VC.prev();
+		    //
+		    // stop detecting gestures
+		    ev.gesture.stopDetect();
+		    //
+		    break;
+		};
+	    };
+
+	    // activate the carousel event handler
+	    $element.hammer({ drag_to_lock_target : true })
+	    .on( 'swipeup swipedown', handleTouch );
+	    //
+	    return VC;
+	    //
+	};//verticalCarousel
+
+
+       
+	var vertTest = verticalCarousel( '#vertCarousel' );
+	vertTest.init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	// enquire code
 	/*
@@ -150,5 +298,5 @@ $(function(){
 	
 	
 	
-    });
+    }); // @end  @annonFunc
 
